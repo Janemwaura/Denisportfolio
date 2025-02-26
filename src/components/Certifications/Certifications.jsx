672e7c2d4035d749022e394bde33c589f52afa57
Janemwaura <mwaura461@gmail.com>
2025-02-26 19:46:11 +0300

    Certifications

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import './Certifications.css';
 const Certifications = () => {
   return (
-    <div className="certifications"> 
-      <div className="section1">
+    <div className="certifications">  
+      <div className="sectionA"> 
       <span>Academic</span>
       <span>Background</span>
       </div>
       <hr />
-      <div className="section2">
+      <div className="sectionB">
       <span>Bachelor of Commerce - Finance option</span>
       <span>Kabarak University</span>
       <span>October 2014 - December 2017</span>
     </div>
-    <div className="section2">
+    <div className="sectionB">
       <span>CPA K </span>
       <span>Achievers School of Professionals</span>
       <span>June 2011 - June 2014</span>
     </div>
-    <div className="section2">
+    <div className="sectionB">
       <span> Computer Application Packages</span>
       <span>Mount Kenya University</span>
       <span> October 2012</span>
     </div>
-    <div className="section2">
+    <div className="sectionB">
       <span>Accounting Packages</span>
       <span>Achievers School of Professionals</span>
       <span>August 2012</span>
--- src/components/References/References.jsx
+++ src/components/References/References.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
 import "./References.css";
+
 const References = () => {
   return (
-    <div>References</div>
-  )
-}
+    <div className="section1">
+      <div className="section2">
+        <span>Professional </span>
+        <span>References</span>
+      </div>
+
+      <hr />
+
+      <div className="references-container">
+        {/* Reference 1 */}
+        <div className="section3">
+          <span>John M Guthiga</span> 
+          <span>Ass Manager Recon- Social Protection</span>
+          <span>Equity Bank Limited</span>
+          <span>0717354199</span>
+          <span>John.Guthiga@equitybank.co.ke</span>
+        </div>
+
+        {/* Reference 2 */}
+        <div className="section3">
+          <span>Martin M Ngotho</span>
+          <span>Senior Finance Accountant</span>
+          <span>ALX (Ken) Ltd</span>
+          <span>0780478816</span>
+          <span>Martinmwangingotho@gmail.com</span>
+        </div>
+
+        {/* Reference 3 */}
+        <div className="section3">
+          <span>Ruth Wambui Kariuki</span>
+          <span>Business Growth and Development Manager</span>
+          <span>Nakuru Westside Mall</span>
+          <span>0721696224</span>
+          <span>Ruth.Kariuki@equitybank.co.ke</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default References;
